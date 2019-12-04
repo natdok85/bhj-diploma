40cdefc6af64de8fa60c591b3310f896392380aa
Natalia Dokuchaeva <natdok85@gmail.com>
2019-12-04 15:30:07 +0300

    first commit diploma file Modal

--- js/ui/Modal.js
+++ js/ui/Modal.js
@@ -1,3 +1,51 @@
+// import { ENETUNREACH } from "constants";
+
+let registerButton = document.querySelector('.menu-item_register');
+
+//открывает модальное окно регистрации
+registerButton.addEventListener('click', e => {
+  e.preventDefault();
+  let modalRegister = document.querySelector('#modal-register');
+  modalRegister.style.display = 'block';
+ 
+})
+
+//обработчики на все закрывающие элементы
+let closeButtons = Array.from(document.querySelectorAll('[data-dismiss]'));
+for(let closeButton of closeButtons) {
+  closeButton.addEventListener('click', e => {
+    e.target.closest('.modal').style.display = '';
+  })
+}
+
+//обработчик на кнопку "зарегистрироваться"
+let formButtons = Array.from(document.querySelectorAll('.btn-primary'));
+for(let formButton of formButtons) {
+  if(formButton.textContent === 'Зарегистрироваться') {
+  formButton.addEventListener('click', e => {
+    e.preventDefault();       
+    let registerForm = document.querySelector('#register-form');
+    formData = new FormData(registerForm);
+    entries = formData.entries();
+    //как добавить все данные из formData в объект?
+    for (let item of entries) {
+      let key = item[0];
+      let value = item[1];        
+        
+      }
+    
+    let dataToSubmit = new Object();
+    dataToSubmit.url = registerForm.getAttribute('action');
+    dataToSubmit.method = registerForm.getAttribute('method');
+    //dataToSubmit.data = 
+       
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://bhj-diplom.letsdocode.ru/user/register.php');    
+    xhr.send(dataToSubmit);    
+    return false;
+  })
+  }  
+}
 /**
  * Класс Modal отвечает за
  * управление всплывающими окнами.
@@ -22,6 +70,7 @@ class Modal {
    * (с помощью метода Modal.onClose)
    * */
   registerEvents() {
+    
 
   }
 
