b471cfae0c03ed1a58b92da70a2e26062ea91859
Natalia Dokuchaeva <natdok85@gmail.com>
2019-12-15 09:28:24 +0300

    first commit createRequest

--- js/api/createRequest.js
+++ js/api/createRequest.js
@@ -2,6 +2,34 @@
  * Основная функция для совершения запросов
  * на сервер.
  * */
-const createRequest = (options = {}) => {
+const createRequest = (options = {url, headers, data, responseType, method}, callback) => {
+    let request = new XMLHttpRequest();
+    function callback(err, response) {
+        console.log(err);
+        console.log(response);
+    }
+    
+    if(options.method === 'GET') {
+        request.open('GET', `https://bhj-diplom.letsdocode.ru?mail=${options.data.mail}&password=${options.data.password}.php`, true)
+    } else {        
+        request.open('POST', 'https://bhj-diplom.letsdocode.ru')
+    };
+    request.addEventListener('readystatechange', function() {
+        if((request.readyState == 4) && (request.status == 200)) {
+            callback(null, request.response);
+        } else {
+            callback(request.response, null);
+        }
+    });
 
-};
+    if(options.method === 'GET') {
+        request.send();
+    } else {
+        let requestData = new FormData();
+        requestData.append('mail', `${options.data.mail}`);
+        requestData.append('password',`${options.data.password}`)
+        request.send(requestData);
+
+    }
+
+}
